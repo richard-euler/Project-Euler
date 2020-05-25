508f7874c8151fc9295e32ce0f731e7ebdc23253
Michael <emailvonmichael@gmail.com>
2020-05-25 15:02:38 +0200

    112 v2

--- 112/main.py
+++ 112/main.py
@@ -15,3 +15,44 @@ is equal to 90%.
 
 Find the least number for which the proportion of bouncy numbers is exactly 99%."""
 
+import timeit
+
+def isBouncy(number):
+    #number = "22100"
+    digitIsIncreasing = []
+    for i in range(0,len(number)-1):
+        current = number[i]
+        next = number[i+1]
+
+        if current < next:
+            digitIsIncreasing.append(1);
+        if current > next:
+            digitIsIncreasing.append(-1);
+        if current == next:
+            digitIsIncreasing.append(0);
+
+    isInceasing = all([j!=-1 for j in digitIsIncreasing])
+    isDecreasing = all([j!=1 for j in digitIsIncreasing])
+    isBouncy = not isInceasing and not isDecreasing
+    return isBouncy
+
+bouncies = []
+percentage = 0.0
+percentageTarget = 0.99
+for n in range(1,200):
+    #start = time.time.perf_counter()
+    if isBouncy(str(n)):
+        bouncies.append(n)
+        percentage += (1-percentage)/n
+    else:
+        percentage += (0-percentage)/n
+    if percentage > percentageTarget :
+        break
+    #end = time.time.perf_counter()
+    #print(end-start)
+
+print("Bouncy number", bouncies[-1], "reaches", "{:.2f}%".format(percentage*100))
+
+# Die Zahl ist um 1 zu hoch!!!
+
+
